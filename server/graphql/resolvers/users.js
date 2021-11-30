297e306a3f68fb17a3c4562f0a06d19bca5a1cbf
Owais <25351744+owais635@users.noreply.github.com>
2021-11-30 03:21:34 -0500

    register user

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -1,11 +1,59 @@
+const bcrypt = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+
+const { validateRegisterInput } = require("../../utils/validators");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
-    register(parent, args, context, info) {
+    async register(parent, args, context, info) {
+      let { username, password, confirmPassword, email } = args.registerInput;
+
       // TODO Validate User Data
-      // TODO make user doesn't already exist
-      // TODO Hash password
+
+      const { errors, valid } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      // make user doesn't already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: { username: "This user name is taken" },
+        });
+      }
+
+      // Hash password
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = jsonwebtoken.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return { ...res._doc, id: res._id, token };
     },
   },
 };
