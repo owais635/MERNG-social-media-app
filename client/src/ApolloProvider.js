fe1f222bc79721450ebaf752a87465b37f93520d
Owais <25351744+owais635@users.noreply.github.com>
2021-11-30 23:45:36 -0500

    add Apollo Provider

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,36 @@
+import React from "react";
+import App from "./App";
+import {
+  InMemoryCache,
+  ApolloClient,
+  createHttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000",
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const Provider = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+export default Provider;
